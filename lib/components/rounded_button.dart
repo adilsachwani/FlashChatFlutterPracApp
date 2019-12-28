c2816d856d29b46bcc88761e67d2ae9cfface4b5
adilsachwani <adilsachwani@gmail.com>
2019-12-28 19:19:39 +0500

    code refactoring

---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  RoundedButton(
+      {@required this.label, @required this.color, @required this.onPress});
+
+  final String label;
+  final Color color;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPress,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            label,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
